645a21d31eafd5a1a7a99fb1b2dcbc98b0b8f4d3
KeilbarG <georgkeilbar@gmail.com>
2019-04-05 10:24:33 +0200

    Add files via upload

--- XFG_VaR_ES.R
+++ XFG_VaR_ES.R
@@ -16,12 +16,12 @@ ES_1 = mean(x[x<quantile(x,0.01)])
 par(mfrow=c(2,2))
 
 #Kernel density plot with VaR and ES (alpha=0.01)
-plot(density(x,bw=0.01,kernel="epanechnikov"),main="Nonparametric estimation, alpha=0.01",xlab="")
+plot(density(x,bw=0.005,kernel="epanechnikov"),main="Nonparametric estimation, alpha=0.01",xlab="")
 abline(v=VaR_1,col="blue")
 abline(v=ES_1,col="red")
 
 #Kernel density plot with VaR and ES (alpha=0.05)
-plot(density(x,bw=0.01,kernel="epanechnikov"),main="Nonparametric estimation, alpha=0.05",xlab="")
+plot(density(x,bw=0.005,kernel="epanechnikov"),main="Nonparametric estimation, alpha=0.05",xlab="")
 abline(v=VaR_5,col="blue")
 abline(v=ES_5,col="red")
 
@@ -43,5 +43,8 @@ plot(dnorm(seq(-0.1,0.1,0.0001),mu,sigma)~seq(-0.1,0.1,0.0001),type="l",main="No
 abline(v=VaR_5_N,col="blue")
 abline(v=ES_5_N,col="red")
 
-pdf("XFG_VaR_ES.pdf",width=6,height=4,paper='special')
 dev.off()
+
+#Calculate skewness and kurtosis
+s = mean((x-mu)^3)/(sigma^3)
+k = mean((x-mu)^4)/(sigma^4)
